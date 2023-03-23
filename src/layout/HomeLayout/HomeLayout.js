52e40361620e83d0d77e86a9b082bcd1cace80d5
hiepxuan <hiepxuan2006@gmail.com>
2023-03-23 21:45:32 +0700

    fix

--- src/layout/HomeLayout/HomeLayout.js
+++ src/layout/HomeLayout/HomeLayout.js
@@ -1,11 +1,10 @@
 import { faSpinner } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { Fragment, useState } from "react"
-import style from "./HomeLayout.module.scss"
 import classNames from "classnames/bind"
+import { Fragment, useState } from "react"
 import Header from "~/app/Header"
-import { MenuBottomMobile } from "~/app/Home/MenuBottomMobile"
 import { Footer } from "~/components/footer/Footer"
+import style from "./HomeLayout.module.scss"
 
 const cx = classNames.bind(style)
 const HomeLayout = ({ children }) => {
